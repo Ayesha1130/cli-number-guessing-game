fb363096247b6a4e543289bb1a5467e66444c554
Ayesha Iqbal <159792850+Ayesha1130@users.noreply.github.com>
2024-03-25 00:53:49 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const ramdomNumber = Math.floor(Math.random() * 6 + 1);
+ 
+const answers = await inquirer.prompt([
+    {
+      name:"userGuessedNumber",
+      type: "number",
+      message: "please guess number between 1-6:",
+    }
+]);
+
+
+if(answers.userGuessedNumber === ramdomNumber){
+    console.log("congtratulations! you guessed right number");
+}else {
+    console.log("0h you guessed wrong number");
+}
